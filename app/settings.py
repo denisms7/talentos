ab88b7566dcfb3517b5f2ecc7f4572d8b823402f
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2025-12-09 16:23:19 -0300

    added

--- app/settings.py
+++ app/settings.py
@@ -51,8 +51,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
 
     'curriculum',
+    'accounts',
 ]
 
 MIDDLEWARE = [
