3bf61997f9ae8814afbdc5ebd48995329542bd72
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2025-12-11 17:23:29 -0300

    added tabelas organizacao

---
+++ static/js/datatables/conf.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('.tabela-classificacao').DataTable({
+        order: [[0, 'desc']],
+        paging: false,
+        searching: false,
+        info: false,
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.7/i18n/pt-BR.json'
+        },
+        columnDefs: [
+            { orderable: false, targets: -1 },
+            { type: 'string', targets: 0 } // força ordenação textual
+        ]
+    });
+});
