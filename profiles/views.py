00f8870a18f85dbdc60515ac16cc5f8a67c91e89
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2025-12-13 14:53:54 -0300

    added

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.db.models import Q
 from django.urls import reverse_lazy
